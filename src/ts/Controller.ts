5e4ce2164a8916fbb0542623f97e5fa8c3845a02
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2023-07-10 10:16:59 +0200

    Added logInDetailsView.ts handler

--- src/ts/Controller.ts
+++ src/ts/Controller.ts
@@ -1,15 +1,20 @@
-import logInModalsView from './Views/logInModalsView'
-import logInDetailsView from './Views/logInDetailsView'
+import logInModalsView from './Views/logInModalsView';
+import logInDetailsView from './Views/logInDetailsView';
+import * as Model from './Model';
 
-const controlShowModel = function () {
+const controlLogIn = async function (logInData: any) {
+	try {
+			const logInMessage = Model.logInDataCheck(logInData);
+			console.log(logInMessage);
+	} catch (error) {
+		console.error(error);
+	}
 };
 
-const init = function ()
-{
-    logInModalsView.addHandlerLogIn()
-    logInModalsView.addHandlerSignUp();
-    logInDetailsView.returnLogInData();
-    logInDetailsView.returnSignUpData();
+const init = function () {
+	logInModalsView.addHandlerLogIn();
+	logInModalsView.addHandlerSignUp();
+	logInDetailsView.addLogInHandler(controlLogIn);
 };
 
 init();
--- src/ts/Views/logInDetailsView.ts
+++ src/ts/Views/logInDetailsView.ts
@@ -1,43 +1,66 @@
 import logInModalsView from './logInModalsView';
 
 class logInDetailsView {
-	#logInUsername = <HTMLInputElement>document.querySelector('#log__in--username');
-	#logInPassword = <HTMLInputElement>document.querySelector('#log__in--password');
-	#signUpUsername = <HTMLInputElement>document.querySelector('#sign__up--username');
-	#signUpPassword = <HTMLInputElement>document.querySelector('#sign__up--password');
-	#signUpPasswordRepeat = <HTMLInputElement>document.querySelector('#sign__up--password__repeat');
+    #logInUsername = <HTMLInputElement>document.querySelector('#log__in--username');
+    #logInPassword = <HTMLInputElement>document.querySelector('#log__in--password');
+    #signUpUsername = <HTMLInputElement>document.querySelector('#sign__up--username');
+    #signUpPassword = <HTMLInputElement>document.querySelector('#sign__up--password');
+    #signUpPasswordRepeat = <HTMLInputElement>document.querySelector('#sign__up--password__repeat');
 
-	returnLogInData() {
+    addLogInHandler(handler: Function) {
 		logInModalsView.logInButtons[1].addEventListener('click', () => {
-			const logInData = this.getData(true);
+			//HACK any
+			const logInData: any = this.#getData(true);
 
-			this.clearData();
+			this.#clearData();
+
+			console.log(logInData);
+
+			handler(logInData);
+		});
+
+		logInModalsView.signUpButtons[1].addEventListener('click', () => {
+			//HACK any
+			const logInData: any = this.#getData(false);
+
+			this.#clearData();
+
+			handler(logInData);
+		});
+    }
+
+	#returnLogInData() {
+		logInModalsView.logInButtons[1].addEventListener('click', () => {
+			//HACK any
+			const logInData: any = this.#getData(true);
+
+            this.#clearData();
+
+            console.log(logInData);
+
+			return logInData;
+		});
+
+		logInModalsView.signUpButtons[1].addEventListener('click', () => {
+			//HACK any
+			const logInData: any = this.#getData(false);
+
+			this.#clearData();
 
 			return logInData;
 		});
 	}
 
-	returnSignUpData() {
-		logInModalsView.signUpButtons[1].addEventListener('click', () => {
-            //HACK
-            const signUpData: any = this.getData(false);
-
-            this.clearData();
-
-            return signUpData.password === signUpData.passwordRepeat ? signUpData : new Error('Passwords do not match')
-
-		});
-	}
-
-	getData(newUser: boolean): Object {
-		return {
-			username: newUser ? this.#logInUsername.value : this.#signUpUsername.value,
-			password: newUser ? this.#logInPassword.value : this.#signUpPassword.value,
-			passwordRepeat: this.#signUpPasswordRepeat.value,
+	#getData(existingUser: boolean): Object {
+        return {
+            existingUser,
+			username: existingUser ? this.#logInUsername.value : this.#signUpUsername.value,
+			password: existingUser ? this.#logInPassword.value : this.#signUpPassword.value,
+            passwordRepeat: this.#signUpPasswordRepeat.value,
 		};
 	}
 
-	clearData(): void {
+	#clearData(): void {
 		this.#logInUsername.value = '';
 		this.#logInPassword.value = '';
 		this.#signUpUsername.value = '';
