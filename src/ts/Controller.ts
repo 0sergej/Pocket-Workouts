e421e2213dd9a5bfe3a47d7f4411c1a49cf6af04
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2023-07-10 14:57:19 +0200

    Added Account Storage
    Accounts can be saved in LacalStorage.

--- src/ts/Controller.ts
+++ src/ts/Controller.ts
@@ -2,9 +2,9 @@ import logInModalsView from './Views/logInModalsView';
 import logInDetailsView from './Views/logInDetailsView';
 import * as Model from './Model';
 
-const controlLogIn = async function (logInData: any) {
-	try {
-			const logInMessage = Model.logInDataCheck(logInData);
+const controlLogIn = function (logInData: any) {
+	const logInResult = Model.logInDataCheck(logInData);
+	if (logInResult === true) logInModalsView.login();
 			console.log(logInMessage);
 	} catch (error) {
 		console.error(error);
--- src/ts/Model.ts
+++ src/ts/Model.ts
@@ -4,13 +4,13 @@ export const logInDataCheck = function (logInData: any) {
 	// Check if the user has an account
 	if (logInData.existingUser === true) {
 		// Check if the user's username and password is correct
-		const storage = localStorage.getItem('logInData');
+		const accountsJSON = localStorage.getItem('logInData[]');
 
 		// If storage exists, check if the user exists.
-		if (storage) {
-			const existingUserData = JSON.parse(storage);
+		if (accountsJSON) {
+			const accountsArr = JSON.parse(accountsJSON);
 
-			return checkIfUserExists(existingUserData, logInData, errMessage);
+			return checkIfUserExists(accountsArr, logInData) === true ? true : errMessage;
 		}
 
 		return errMessage;
@@ -18,24 +18,50 @@ export const logInDataCheck = function (logInData: any) {
 	// If the user is new
 	else if (logInData.existingUser === false) {
 		// Check if password's match
-		const message = checkPasswords(logInData);
+		const logInResult = checkPasswords(logInData);
 		// Save User's Data
-		localStorage.setItem('logInData', JSON.stringify(logInData));
+		makeUser(logInData);
 
-		return message;
+		return logInResult;
 	}
 };
 
 //HACK any
-const checkIfUserExists = function (userData: any, logInData: any, errMessage: string) {
-	if (userData.username === logInData.username && userData.password === logInData.password) return `Logged in as ${userData.username}.`;
+const checkIfUserExists = function (accountsArr: any, logInData: any) {
+	let accountExists: boolean = false;
+	//HACK any
+	accountsArr.map((account: any) => {
+		if (account.username === logInData.username && account.password === logInData.password) {
+			console.log(1);
+			accountExists = true;
+		}
+	});
 
-	return errMessage;
+	if (accountExists) return accountExists;
 };
 
 //HACK any
-const checkPasswords = function (userData: any) {
-	if (userData.password !== userData.passwordRepeat) return new Error('Passwords do not match. Please check your password and try again.');
+const checkPasswords = function (accountsArr: any) {
+	if (accountsArr.password !== accountsArr.passwordRepeat) return 'Passwords do not match. Please check your password and try again.';
 
-	return `Signed in as ${userData.username}.`;
+	return true;
+};
+
+//HACK any
+const makeUser = function (logInData: any) {
+	// Get an old Data
+	const accountsJSON = localStorage.getItem('logInData[]');
+
+	// Check if there is any old data
+	if (accountsJSON) {
+		// Get old data
+		const accountsArr: Array<any> = [];
+		accountsArr.push(...JSON.parse(accountsJSON));
+		// Add new user data
+		accountsArr.push(logInData);
+		localStorage.setItem('logInData[]', JSON.stringify(accountsArr));
+		return;
+	}
+
+	localStorage.setItem('logInData[]', JSON.stringify([logInData]));
 };
