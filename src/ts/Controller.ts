bc8473b8946403a043df4d803fc37ee7b2a48bc1
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2023-09-09 20:06:01 +0200

    Minor

--- src/ts/Controller.ts
+++ src/ts/Controller.ts
@@ -1,15 +1,18 @@
 import logInModalsView from './Views/logInModalsView';
 import logInDetailsView from './Views/logInDetailsView';
 import * as Model from './Model';
+import errorMessageView from './Views/errorMessageView';
+import slidingImagesView from './Views/slidingImagesView';
 
 const controlLogIn = function (logInData: any) {
-    const logInResult = Model.logInDataCheck(logInData);
-    console.log(logInResult);
-    if (logInResult === true)
-    {
-        logInModalsView.login();
-        logInModalsView.logInHeader(logInData)
-    }
+	const logInResult = Model.logInDataCheck(logInData);
+	if (logInResult === true) {
+		logInModalsView.login();
+		logInModalsView.logInHeader(logInData);
+	}
+	if (logInResult !== true && typeof logInResult !== 'boolean') {
+		errorMessageView.showErrorMessageBox(logInResult);
+	}
 };
 
 const init = function () {
