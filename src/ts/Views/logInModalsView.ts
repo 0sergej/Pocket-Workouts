8c61c506a448f1e726bb78dfe4cda1252573a560
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2023-09-09 20:04:32 +0200

    Code clean-up and new closing button

--- src/ts/Views/logInModalsView.ts
+++ src/ts/Views/logInModalsView.ts
@@ -4,7 +4,9 @@ class logInModalsView {
 	signOutButton = document.querySelector('#sign__out--btn');
 	logInModal = document.querySelector('#log__in--modal');
 	signUpModal = document.querySelector('#sign__up--modal');
-	logInHeaderContainter = document.querySelector('#log__in---header');
+	logInHeaderContainter = document.querySelector('#log__in--header');
+	#closingXLogIn = document.getElementById('log__in--closing__X');
+	#closingXSignUp = document.getElementById('sign__up--closing__X');
 
 	addHandlerLogIn() {
 		this.#bindButtons(this.logInButtons, () => this.#showModal(this.logInModal, this.signUpModal));
@@ -18,6 +20,18 @@ class logInModalsView {
 		this.signOutButton?.addEventListener('click', () => {
 			this.#logOut();
 			this.logInHeaderContainter?.classList.add('hidden');
+		});
+	}
+
+	addHandlerCloseLogIn() {
+		this.#closingXLogIn?.addEventListener('click', () => {
+			this.#closeModal(this.logInModal);
+		});
+	}
+
+	addHandlerCloseSignUp() {
+		this.#closingXSignUp?.addEventListener('click', () => {
+			this.#closeModal(this.signUpModal);
 		});
 	}
 
@@ -46,13 +60,20 @@ class logInModalsView {
 	}
 
 	#showModal(showModal: Element | null, hideModal: Element | null) {
-		hideModal?.classList.remove('modal');
-		showModal?.classList.toggle('modal');
+		showModal?.classList.toggle('fade-out-element');
+		showModal?.classList.toggle('fade-in-element');
+		hideModal?.classList.toggle('fade-in-element', false);
+		hideModal?.classList.toggle('fade-out-element', true);
 	}
 
 	#bindButtons(btnType: Element[], modalType: any) {
 		btnType.map((btn) => btn.addEventListener('click', modalType?.bind(this)));
 	}
+
+	#closeModal(modal: Element | null) {
+		modal?.classList.toggle('fade-in-element', false);
+		modal?.classList.toggle('fade-out-element', true);
+}
 }
 
 export default new logInModalsView();
